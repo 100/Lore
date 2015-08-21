a46163fd9c0ae51418e5ba928e90bb215f4bff44
d-soni <devinsoni1010@gmail.com>
2015-08-21 17:25:30 -0400

    fixed naming error

--- twitter.py
+++ twitter.py
@@ -22,14 +22,14 @@ def createTwiiterSource(search): #searches 500 tweets based on results from 'sea
 def createTweets(source, num): #num is number of tweets to create
     words = createDict(source, 2) #state size of 2 allows for more combinations as tweets are small
     tweets = []
-    api = AlchemyAPI()
+    alchemyAPI = AlchemyAPI()
     for x in range(0, num): #at most 50% chance of using a hashtag
         if randint(0,1) == 0:
             tweet = generateText(words, 2, choice(range(100,140)))
             tweets.append(tweet)
         else:
             tweet = generateText(words, 2, choice(range(80,120)))
-            response = api.concepts('text', tweet)
+            response = alchemyAPI.concepts('text', tweet)
             if response['status'] == 'OK':
                 hashtag = " #" + response['concepts'][0]['text'].replace(" ", "")
                 if len(hashtag) <= 140 - len(tweet):
