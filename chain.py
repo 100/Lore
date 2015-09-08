b44d41d720062a5be49b693f0514d78326342db5
d-soni <devinsoni1010@gmail.com>
2015-09-07 23:09:10 -0400

    fixed most errors in chain creation

--- chain.py
+++ chain.py
@@ -4,29 +4,34 @@ def createDict(source, size): #size specifies the number of words in each state
     splitSource = source.split(" ")
     words = {}
     for index, word in enumerate(splitSource):
-        try:
-            tempList = splitSource[index: index+size+1]
-        except IndexError: #case of it not dividing evenly
+        if len(splitSource) < index + size + 1: #case of it not dividing evenly
             break
-        if tempList[0:size] not in words:
-            words[tempList[0:size]] = [tempList[size]]
+        tempList = splitSource[index: index+size+1]
+        if tuple(tempList[0:size]) not in words:
+            words[tuple(tempList[0:size])] = [tempList[size]]
         else:
-            words[tempList[0:size]].append(tempList[size])
+            words[tuple(tempList[0:size])].append(tempList[size])
     return words
 
 def generateText(words, size, length): #size specifies the number of words in each state, length is max characters
-    upper = [key for key in words.keys() if key[0][0].isupper()]
+    upper = []
+    for key in words.keys():
+        try:
+            if list(key)[0][0][0].isupper():
+                upper.append(list(key))
+        except IndexError:
+            pass #empty strings
     if len(upper) > 0:
         start = choice(upper)
     else: #if no words uppercase, pick entirely by random
-        start = choice(words.keys())
+        start = list(choice(words.keys()))
     text = start
     currentLength = sum([len(word) for word in text]) + (size-1)
     while True:
         current = text[-size:]
-        selected = choice(words[current])
+        selected = choice(words[tuple(current)])
         text.append(selected)
-        if len(currentLength + selected)+1 <= length:
+        if len(selected) + currentLength + 1 <= length:
             currentLength = currentLength + len(selected) + 1 # +1 is to account for spaces later
         else:
             break
