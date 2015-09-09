092275e8bf9a5c00721c6c8c53097144102a38e4
d-soni <devinsoni1010@gmail.com>
2015-09-08 21:41:50 -0400

    added config file

---
+++ app/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import Form
+from wtforms import IntegerField, SelectField, validators
+
+#Integer field to get characters
+
+#SelectField dropdown of text options for sources, pulled from database
---
+++ config.py
@@ -0,0 +1,4 @@
+from private import SECRET_KEY as secret
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = secret
